de43cacd137bb42dc224ac93556f9ad11fce4e7a
Sasha <sashapysmenny@gmail.com>
2021-09-13 17:00:57 +0300

    refactoring

--- app/src/main/java/com/example/simulation_kotlin/Rocket.kt
+++ app/src/main/java/com/example/simulation_kotlin/Rocket.kt
@@ -4,17 +4,11 @@ open class Rocket(val cost: Int, val weight: Int, val maxWeight: Int,
                   val chanceOfLaunchExplosion: Double, val chanceOfLandingCrash: Double) : SpaceShip {
     var currentWeight = weight;
 
-    override fun launch(): Boolean {
-        return true
-    }
+    override fun launch(): Boolean = true
 
-    override fun land(): Boolean {
-        return true
-    }
+    override fun land(): Boolean = true
 
-    override fun canCarry(item: Item?): Boolean {
-        return currentWeight + weight <= maxWeight
-    }
+    override fun canCarry(item: Item?): Boolean = currentWeight + weight <= maxWeight
 
     override fun carry(item: Item?): Int {
         currentWeight += item!!.weight
